953993165a9c7a20147d7933e54ca851c17ed909
SHAHANA SHIRIN <135540357+Shahaaashirii@users.noreply.github.com>
2024-08-21 18:22:03 +0530

    Add files via upload

---
+++ asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for dlai_yoga_pose project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dlai_yoga_pose.settings')
+
+application = get_asgi_application()
